7d3c9eb8ac86ee9c5a4525c1c0db4af257d76a02
caymenp <109010237+caymenp@users.noreply.github.com>
2023-03-20 12:12:25 -0500

    Update checkForUpdate.js

--- api/routes/checkForUpdate.js
+++ api/routes/checkForUpdate.js
@@ -3,17 +3,16 @@ const itemModel = require("../models/itemModel");
 
 async function scheduledRefresh() {
   console.log("Function: 'Scheduled Refresh' Starting!");
-  let allItems = await itemModel.find({});
-  let payload;
+  let allItems = await itemModel.find();
   for (const item of allItems) {
-    payload = {
+    let payload = {
       _id: item._id,
       productURL: item.productURL,
       userEmail: item.userEmail,
       recentPrice:
         item.productPriceUsed[item.productPriceUsed.length - 1].usedPrice,
     };
-    let res = await axios
+    await axios
       .post("https://api.amzused.com/app/refreshItem", payload)
       .then((response) => {
         console.log("Updating");
@@ -21,6 +20,8 @@ async function scheduledRefresh() {
       .catch((error) => {
         console.log(error);
       });
+    console.log("going to next");
   }
 }
+
 module.exports = { scheduledRefresh };
