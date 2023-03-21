3e82c38dcc15fc3a54fe08c6420c39a75d1661f7
caymenp <109010237+caymenp@users.noreply.github.com>
2023-03-20 19:03:42 -0500

    Fixing automatic function

--- api/routes/checkForUpdate.js
+++ api/routes/checkForUpdate.js
@@ -1,22 +1,32 @@
 const axios = require("axios");
 const itemModel = require("../models/itemModel");
 
+async function makeCall(payload) {
+  console.log("Sending! ", payload);
+  const res = await axios
+    .post("https://api.amzused.com/app/refreshItem", payload)
+    .then(async (res) => {
+      console.log("Waiting For Data");
+      await res.data;
+      console.log("Got the data!: ", res.data);
+      return res.data;
+    });
+}
+
 async function scheduledRefresh() {
-  let allItems = await itemModel.find();
-  for (const item of allItems) {
+  let allItems = await itemModel.find({});
+  for (let i = 0; i < allItems.length; i++) {
+    console.log("Making Payload");
     let payload = {
-      _id: item._id,
-      productURL: item.productURL,
-      userEmail: item.userEmail,
+      _id: allItems[i]._id.toString(),
+      productURL: allItems[i].productURL,
+      userEmail: allItems[i].userEmail,
       recentPrice:
-        item.productPriceUsed[item.productPriceUsed.length - 1].usedPrice,
+        allItems[i].productPriceUsed[allItems[i].productPriceUsed.length - 1]
+          .usedPrice,
     };
-    await axios
-      .post("https://api.amzused.com/app/refreshItem", payload)
-      .then((response) => {})
-      .catch((error) => {
-        console.log(error);
-      });
+    console.log("Sending to API function: ", payload._id);
+    await makeCall(payload);
   }
 }
 
--- api/routes/getItemData.js
+++ api/routes/getItemData.js
@@ -54,10 +54,16 @@ function cheerioProd(HTMLbody) {
   const prodURL = $('link[rel="canonical"]').attr("href");
 
   //Getting New Price String
-  const newPriceString = $("#twister-plus-price-data-price")
-    .attr("value")
-    .replace("$", "")
-    .replace(",", "");
+  let newPriceString = $("#twister-plus-price-data-price").attr("value");
+
+  if (newPriceString.indexOf("$") >= 0) {
+    newPriceString = newPriceString.replace("$", "");
+  }
+
+  if (newPriceString.indexOf(",") >= 0) {
+    newPriceString = newPriceString.replace(",", "");
+  }
+
   //Splitting at the $, resulting in just the $ amount, and transversing it into a number with '+'
   const newPrice = Number(newPriceString);
   //Getting Lowest Price String
